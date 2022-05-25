9eb7dfab197ff9ef9429bf0ab2cb3bd49dec4225
loveYcr7 <67047412+loveYcr7@users.noreply.github.com>
2022-05-25 15:55:17 +0800

    Update main.go
    首次记录

--- simple-最后一个单词的长度/main.go
+++ simple-最后一个单词的长度/main.go
@@ -1,1 +1,30 @@
+package main
+
+import "fmt"
+
+func lengthOfLastWord(s string) int {
+	length := len(s)
+	u := length - 1
+	var sum int
+	sum = 0
+  //循环排空
+	for s[u] == ' ' {
+		u--
+
+	}
+  //开始计算最后一个
+	for s[u] != ' ' {
+		u -- 
+		sum++
+	}
+	
+	return sum
+
+}
+
+func main() {
+	s := " trsr yo lll     "
+	k := lengthOfLastWord(s)
+	fmt.Println(k)
+}
 
